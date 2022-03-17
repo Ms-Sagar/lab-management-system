67c3a62640a4eb174328abb4930bbb2e9c8ebf18
Ms-Sagar <92791132+Ms-Sagar@users.noreply.github.com>
2022-03-17 22:03:24 +0530

    Create student.model.js

---
+++ backend/models/student.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Schema = mongoose.Schema;
+
+
+const StudentSchema = new Schema({
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: [true, "Please provide email address"],
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+    },
+    subject: { type: String, required: true },
+    attendence: { type: Number, required: true },
+    total:{type:Number,required:true},
+  }, {
+    timestamps: true,
+  });
+  
+  StudentSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      next();
+    }
+  
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+  
+  StudentSchema.methods.matchPassword =  function (password,cb) {
+    return  bcrypt.compare(password, this.password,cb);
+  };
+  
+  StudentSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+  };
+  
+const Student = mongoose.model('Student', StudentSchema);
+
+module.exports = Student;
